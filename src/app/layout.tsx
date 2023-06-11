71070d744dd77d1940b90a92cefe2dd005c49e43
Mateusz <mateusz4k@outlook.com>
2023-06-11 19:28:46 +0200

    deleted unused comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,18 +7,6 @@ import { MoviesProvider } from '@/components/store/MoviesProvider'
 
 const outfit = Outfit({ subsets: ['latin'] })
 
-// <!DOCTYPE html>
-// <html lang="pl">
-// <head>
-// 	<meta charset="UTF-8" />
-// 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-// 	<title>Document</title>
-// </head>
-// <body>
-
-// </body>
-// </html>
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
